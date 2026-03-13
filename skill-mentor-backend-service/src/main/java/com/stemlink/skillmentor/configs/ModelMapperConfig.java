94dda36e72441e5164620962ff803f37d6138c23
Sandali.js <sandalisandagomi@gmail.com>
2026-03-14 03:45:02 +0530

    feat: configure ModelMapper for Mentor and Subject DTO mappings

--- skill-mentor-backend-service/src/main/java/com/stemlink/skillmentor/configs/ModelMapperConfig.java
+++ skill-mentor-backend-service/src/main/java/com/stemlink/skillmentor/configs/ModelMapperConfig.java
@@ -1,5 +1,9 @@
 package com.stemlink.skillmentor.configs;
 
+import com.stemlink.skillmentor.dto.response.MentorResponseDTO;
+import com.stemlink.skillmentor.dto.response.SubjectResponseDTO;
+import com.stemlink.skillmentor.entities.Mentor;
+import com.stemlink.skillmentor.entities.Subject;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +17,8 @@ public class ModelMapperConfig {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setSkipNullEnabled(true);
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        modelMapper.typeMap(Mentor.class, MentorResponseDTO.class);
+        modelMapper.typeMap(Subject.class, SubjectResponseDTO.class);
         return modelMapper;
     }
 }
