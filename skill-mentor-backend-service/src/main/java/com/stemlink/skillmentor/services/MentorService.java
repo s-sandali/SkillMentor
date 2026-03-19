c71fed68b71ec24fa717ee6fe956937be05bb582
Sandali.js <sandalisandagomi@gmail.com>
2026-03-19 17:48:11 +0530

    feat: update MentorService to return MentorResponseDTO for mentor-related methods

--- skill-mentor-backend-service/src/main/java/com/stemlink/skillmentor/services/MentorService.java
+++ skill-mentor-backend-service/src/main/java/com/stemlink/skillmentor/services/MentorService.java
@@ -1,16 +1,18 @@
 package com.stemlink.skillmentor.services;
 
 import com.stemlink.skillmentor.dto.AdminMentorRequestDTO;
+import com.stemlink.skillmentor.dto.response.MentorProfileResponseDTO;
 import com.stemlink.skillmentor.dto.response.MentorResponseDTO;
 import com.stemlink.skillmentor.entities.Mentor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface MentorService {
-    Mentor createNewMentor(Mentor mentor);
+    MentorResponseDTO createNewMentor(Mentor mentor);
     MentorResponseDTO createAdminMentor(AdminMentorRequestDTO requestDTO);
-    Page<Mentor> getAllMentors(String name, Pageable pageable);
-    Mentor getMentorById(Long id);
-    Mentor updateMentorById(Long id, Mentor updatedMentor, String requestingClerkId, boolean isAdmin);
+    Page<MentorResponseDTO> getAllMentors(String name, Pageable pageable);
+    MentorResponseDTO getMentorById(Long id);
+    MentorResponseDTO updateMentorById(Long id, Mentor updatedMentor, String requestingClerkId, boolean isAdmin);
     void deleteMentor(Long id);
+    MentorProfileResponseDTO getMentorProfile(Long mentorId);
 }
